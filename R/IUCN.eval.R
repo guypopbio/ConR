60c3f256a71be91279ea876f3987738cf890bcaf
guypopbio <74257817+guypopbio@users.noreply.github.com>
2020-11-10 17:13:14 +0100

    Update IUCN.eval.R

--- R/IUCN.eval.R
+++ R/IUCN.eval.R
@@ -1,37 +1,38 @@
 
 #' @title Preliminary conservation status assessment following IUCN Criterion B
-#' 
+#' Kommentar
+
 #' @description Given a dataframe of georeferenced occurrences of one, or more, taxa, this
 #' function provide statistics values (Extent of Occurrence, Area of Occupancy,
 #' number of locations, number of subpopulations) and provide a preliminary
 #' conservation status following Criterion B of IUCN.  A graphical map output
 #' is also available.
-#' 
+#'
 #' @details
 #' \strong{Input} as a \code{dataframe} should have the following structure:
-#' 
+#'
 #' \strong{It is mandatory to respect field positions, but field names do not
 #' matter}
-#' 
+#'
 #' \tabular{ccccc}{ [,1] \tab ddlat \tab numeric, latitude (in decimal
 #' degrees)\cr [,2] \tab ddlon \tab numeric, longitude (in decimal degrees)\cr
 #' [,3] \tab tax \tab character or factor, taxa names\cr [,4] \tab family \tab
 #' character, optional field indicating higher taxonomic rank\cr [,5] \tab coly
 #' \tab numeric, optional field indicating collection year\cr }
-#' 
+#'
 #' \code{coly} and \code{family} are optional fields
-#' 
+#'
 #' If the optional field named 'family' is provided, indicating higher
 #' taxonomic rank, this will be displayed in the title of the map if
 #' \code{DrawMap} is 'TRUE'.
-#' 
+#'
 #' If the optional field named 'coly' is provided, indicating collection year,
 #' a sub-graph in the map will be displayed (if \code{DrawMap} and
 #' \code{add.legend} are both TRUE) showing a barplot of collection year
-#' 
+#'
 #' \strong{Starting position of the raster used for estimating the Area Of
-#' Occupancy}\cr 
-#' 
+#' Occupancy}\cr
+#'
 #' Different starting position of the raster used for estimate
 #' the AOO may provide different number of occupied cells. Hence, by default, 4
 #' different translations of the raster is done (fixed increment of 1/4
@@ -39,9 +40,9 @@
 #' for estimating AOO. It is also possible to define a given number of random
 #' starting position of the raster using the argument
 #' \code{nbe.rep.rast.AOO}\cr
-#' 
-#' \strong{Estimating number of locations}\cr 
-#' 
+#'
+#' \strong{Estimating number of locations}\cr
+#'
 #' Locations are estimated by
 #' overlaying a grid of a given resolution (see \code{Cell_size_locations} for
 #' specifying the resolution). The number of locations is simply the number of
@@ -49,14 +50,14 @@
 #' minimize the number of locations (several translation of the grid are
 #' performed and the one providing the minimum number of occupied cells is
 #' provided).
-#' 
+#'
 #' \strong{Taking into account protected area for estimating the number of
 #' locations}\cr A location is defined by the IUCN as a "geographically or
 #' ecologically distinct area in which a single threatening event can affect
 #' all individuals of the taxon". A simple way to include threat level is to
 #' rely on a map of protected areas and assume that populations within and
 #' outside protected areas are under different threat level.\cr
-#' 
+#'
 #' If a map of protected area is provided, this one is used for estimating the
 #' number of locations by the following procedure:\cr - if
 #' \code{method_protected_area} is "no_more_than_one", all occurrences within a
@@ -67,22 +68,22 @@
 #' locations will be estimated by the overlaying grid as described above, but
 #' by considering differently occurrences outside and inside protected area.
 #' See the vignette for illustration. \cr
-#' 
+#'
 #' The protected areas layers should be given as as
 #' \code{SpatialPolygonsDataFrame} in \code{protec.areas}. The
 #' \code{ID_shape_PA} should also be given and should represent the unique ID
 #' of each protected area in the provided shapefile. This can be checked by the
 #' following code:
-#' 
+#'
 #' \code{colnames(ProtectedAreas@data)} Where ProtectedAreas is the name of
 #' your shapefile.
-#' 
+#'
 #' \strong{Limitation in the estimations of EOO}\cr
-#' 
+#'
 #' For a species whose occurrences span more than 180 degrees, EOO is not
 #' computed. This is the case for example for species whose distribution span
 #' the 180th meridian.
-#' 
+#'
 #' @param DATA a \code{dataframe} or an object of class \code{spgeoIN} see
 #' \url{https://github.com/azizka/speciesgeocodeR}. See Details
 #' @param country_map a \code{SpatialPolygonsDataFrame} or
@@ -164,17 +165,17 @@
 #' execution. By default, it is 2
 #' @return A \code{dataframe} if 'export_shp' is FALSE. A \code{list} if
 #' 'export_shp' is TRUE.
-#' 
-#' If a \code{list}, three elements are provided: 
-#' 
+#'
+#' If a \code{list}, three elements are provided:
+#'
 #' \enumerate{
 #'   \item a \code{dataframe} with results (see field description below)
 #'   \item a list of \code{SpatialPolygons} used for EOO computation
 #'   \item a list of \code{SpatialPolygons} used for subpopulations
 #' }
-#' 
+#'
 #' The \code{dataframe} has as many rows as taxa and the following fields:
-#' 
+#'
 #' \tabular{cccccc}{ [,1] \tab EOO \tab numeric, EOO (square kilometers)\cr
 #' [,2] \tab AOO \tab numeric, AOO (square kilometers)\cr [,3] \tab
 #' Nbe_unique_occ. \tab numeric, Number of unique occurrences\cr [,4] \tab
@@ -186,48 +187,48 @@
 #' Category_EOO \tab character, IUCN threat category according to Criterion B
 #' ignoring AOO\cr }
 #' @author Gilles Dauby
-#' 
+#'
 #' \email{gildauby@@gmail.com}
 #' @seealso \url{https://CRAN.R-project.org/package=biogeo}
-#' 
+#'
 #' \url{https://github.com/azizka/speciesgeocodeR}
 #' @references Gaston KJ & Fuller AF, 2009, The sizes of species'geographic
 #' ranges, Journal of Applied Ecology, 49 1-9
-#' 
+#'
 #' IUCN Standards and Petitions Subcommitte, 2010, Guidelines for Using the
 #' IUCN Red List Categories and Criteria.
 #' \url{https://www.iucnredlist.org/resources/categories-and-criteria}
-#' 
+#'
 #' Rivers CM, Bachman SP & Meagher TR, 2010, Subpopulations, locations and
 #' fragmentation: applying IUCN red list criteria to herbarium specimen data,
 #' Biodiversity Conservation 19:2071-2085
 #' @examples
-#' 
-#' 
+#'
+#'
 #' data(dataset.ex)
 #' data(land)
 #' \dontrun{
 #' Results <- IUCN.eval(dataset.ex, country_map=land)
-#' ## A directory has been created in your working directory 
+#' ## A directory has been created in your working directory
 #' and maps for each species has been produced
-#' 
+#'
 #' ### The method for computing locations is a sliding scale:
 #' ## the grid resolution will be 0.05*the maximum distance separating occurrences
-#' Results <- IUCN.eval(dataset.ex, 
+#' Results <- IUCN.eval(dataset.ex,
 #'                      country_map=land, Cell_size_locations=10,
 #'                      Resol_sub_pop = 5, Cell_size_AOO = 4, method_locations="sliding scale")
-#' 		}			 
+#' 		}
 #' \dontrun{
 #' ## Install speciesgeocodeR package for an example with their lemurs dataset
 #' library(speciesgeocodeR)
 #' data("lemurs_in")
-#' 
+#'
 #' Results <- IUCN.eval(lemurs_in, DrawMap=FALSE, country_map=land, SubPop=FALSE)
-#' 
-#' 
-#' 
+#'
+#'
+#'
 #' }
-#' 
+#'
 #' @importFrom tibble is_tibble
 #' @importFrom writexl write_xlsx
 #' @importFrom utils txtProgressBar setTxtProgressBar
@@ -236,9 +237,9 @@
 #' @importFrom foreach %dopar% %do% foreach
 #' @importFrom rnaturalearth ne_countries
 #' @importFrom sf st_transform sf_project st_crs st_cast
-#' 
-#' 
-#' 
+#'
+#'
+#'
 #' @export
 IUCN.eval <- function (DATA,
                        country_map = NULL,
@@ -270,17 +271,17 @@ IUCN.eval <- function (DATA,
                        write_file_option = "excel",
                        parallel = FALSE,
                        NbeCores = 2) {
-  
+
   .Deprecated(msg = "'IUCN.eval' is deprecated, please 'criterion_B' instead")
-  
+
   if(class(DATA)[1]=="spgeoIN") {
     DATA_2 <- cbind(DATA$species_coordinates, DATA$identifier)
     DATA <- DATA_2[,c(2,1,3)]
   }
   colnames(DATA)[1:3] <- c("ddlat","ddlon","tax")
-  
+
   if(tibble::is_tibble(DATA)) DATA <- as.data.frame(DATA)
-  
+
   if (any(is.na(DATA[, 1:2]))) {
     length(which(rowMeans(is.na(DATA[, 1:2])) > 0))
     unique(DATA[which(rowMeans(is.na(DATA[, 1:2])) > 0), 3])
@@ -296,14 +297,14 @@ IUCN.eval <- function (DATA,
     DATA <- DATA[which(!is.na(DATA[, 1])), ]
     DATA <- DATA[which(!is.na(DATA[, 2])), ]
   }
-  
+
   if(is.factor(DATA[,"tax"])) DATA[,"tax"] <- as.character(DATA[,"tax"])
-  
-  
+
+
   if(!is.numeric(DATA[,1]) || !is.numeric(DATA[,2]) ) {
     if(!is.double(DATA[,1]) || !is.double(DATA[,2])) stop("coordinates in DATA should be numeric")
   }
-  
+
   if (any(DATA[, 1] > 180) ||
       any(DATA[, 1] < -180) ||
       any(DATA[, 2] < -180) ||
@@ -312,50 +313,50 @@ IUCN.eval <- function (DATA,
   if (!is.null(country_map))
     if (!class(country_map) == "SpatialPolygonsDataFrame")
       stop("Country_map should be a spatialpolygondataframe")
-  
+
   if (!is.null(protec.areas)) {
     if (!class(protec.areas) == "SpatialPolygonsDataFrame")
       stop("protec.areas should be a spatialpolygondataframe")
-    
+
     if (!any(colnames(protec.areas@data) %in% ID_shape_PA))
       stop(
         "Check argument ID_shape_PA because selected ID field in the protected area shapefile does not exist"
       )
   }
-  
+
   if (is.null(country_map)) {
     country_map <-
       rnaturalearth::ne_countries(scale = 50, returnclass = "sp")
-    
+
     # data('land', package='ConR', envir=environment())
     # land <- get("land", envir=environment())
     # country_map <- land
   }else{
-    
+
     country_map <-
       suppressWarnings(rgeos::gBuffer(country_map, byid = TRUE, width = 0))
-    
+
   }
-  
+
   if (!is.null(protec.areas)) {
     if (!sp::identicalCRS(protec.areas, country_map)) {
-      
-      sp::proj4string(protec.areas) <- 
+
+      sp::proj4string(protec.areas) <-
         sp::CRS(SRS_string = 'EPSG:4326')
       sp::proj4string(country_map) <-
         sp::CRS(SRS_string = 'EPSG:4326')
-      
+
     }
     # raster::crs(protec.areas) <- raster::crs(country_map)
   }
-  
+
   if(length(grep("[?]", DATA[,3]))>0) DATA[,3] <- gsub("[?]", "_", DATA[,3])
   if(length(grep("[/]", DATA[,3]))>0) DATA[,3] <- gsub("[/]", "_", DATA[,3])
-  
+
   #####
-  
+
   list_data <- split(DATA, f = DATA$tax)
-  
+
   if(map_pdf){
     if(!is.null(file_name)) {
       NAME_FILE <- file_name
@@ -366,26 +367,26 @@ IUCN.eval <- function (DATA,
     dir.create(file.path(paste(
       getwd(), paste("/", DIRECTORY_NAME, "results_map", sep = ""), sep = ""
     )), showWarnings = FALSE)
-    
+
     pdf(paste(paste(
       getwd(), paste("/", DIRECTORY_NAME, "results_map", sep = ""), sep = ""
     ), "/", "results.pdf", sep = ""),
     width = 25,
     height = 25)
   }
-  
-  
-  
+
+
+
   # library(progressr)
   # library(doFuture)
-  # 
+  #
   # registerDoFuture()
   # plan(multisession)
-  # 
+  #
   # handlers("txtprogressbar")
-  # 
+  #
   # xs <- 1:10
-  # 
+  #
   # with_progress({
   #   p <- progressor(along = xs) ## create a 5-step progressor
   #   y <- foreach(x = xs) %dopar% {
@@ -394,13 +395,13 @@ IUCN.eval <- function (DATA,
   #     sqrt(x)
   #   }
   # })
-  # 
-  
+  #
+
   if(parallel) {
-    
+
     cl <- snow::makeSOCKcluster(NbeCores)
     doSNOW::registerDoSNOW(cl)
-    
+
     # registerDoParallel(NbeCores)
     message('Parallel running with ',
             NbeCores, ' cores')
@@ -408,56 +409,56 @@ IUCN.eval <- function (DATA,
   }else{
     `%d%` <- foreach::`%do%`
   }
-  
+
   x <- NULL
-  pb <- 
+  pb <-
     utils::txtProgressBar(min = 0, max = length(list_data), style = 3)
   progress <- function(n)
     utils::setTxtProgressBar(pb, n)
   opts <- list(progress = progress)
-  Results <- 
+  Results <-
     foreach::foreach(x = 1:length(list_data),
                      .options.snow = opts) %d% {
-                       
+
                        if (!parallel)
                          utils::setTxtProgressBar(pb, x)
-                       
-                       res <- 
+
+                       res <-
                          IUCN.comp(DATA = list_data[[x]],
-                                    NamesSp = names(list_data)[x], 
-                                    DrawMap = DrawMap, 
-                                    exclude.area = exclude.area, 
-                                    write_shp = write_shp, 
-                                    method_protected_area = method_protected_area, 
-                                    Cell_size_AOO = Cell_size_AOO, 
-                                    Cell_size_locations = Cell_size_locations, 
-                                    Resol_sub_pop = Resol_sub_pop, 
-                                    method_locations = method_locations, 
-                                    Rel_cell_size = Rel_cell_size, 
-                                    poly_borders = country_map, 
-                                    file_name = file_name, 
-                                    buff_width = buff_width, 
-                                    map_pdf = map_pdf, 
-                                    ID_shape_PA = ID_shape_PA, 
-                                    SubPop = SubPop, 
-                                    protec.areas = protec.areas, 
+                                    NamesSp = names(list_data)[x],
+                                    DrawMap = DrawMap,
+                                    exclude.area = exclude.area,
+                                    write_shp = write_shp,
+                                    method_protected_area = method_protected_area,
+                                    Cell_size_AOO = Cell_size_AOO,
+                                    Cell_size_locations = Cell_size_locations,
+                                    Resol_sub_pop = Resol_sub_pop,
+                                    method_locations = method_locations,
+                                    Rel_cell_size = Rel_cell_size,
+                                    poly_borders = country_map,
+                                    file_name = file_name,
+                                    buff_width = buff_width,
+                                    map_pdf = map_pdf,
+                                    ID_shape_PA = ID_shape_PA,
+                                    SubPop = SubPop,
+                                    protec.areas = protec.areas,
                                     add.legend = add.legend,
-                                    alpha = alpha, 
-                                    buff.alpha = buff.alpha, 
-                                    method.range = method.range, 
-                                    nbe.rep.rast.AOO = nbe.rep.rast.AOO, 
+                                    alpha = alpha,
+                                    buff.alpha = buff.alpha,
+                                    method.range = method.range,
+                                    nbe.rep.rast.AOO = nbe.rep.rast.AOO,
                                     showWarnings = showWarnings,
                                     MinMax = c(min(list_data[[x]][,2]), max(list_data[[x]][,2]), min(list_data[[x]][,1]), max(list_data[[x]][,1])))
-                       
-                       
+
+
                        res
                      }
-  
+
   if(parallel) snow::stopCluster(cl)
   close(pb)
-  
+
   if(map_pdf) grDevices::dev.off()
-  
+
   Results_short <- lapply(Results, `[`, 1)
   Results_short <-
     lapply(
@@ -465,42 +466,42 @@ IUCN.eval <- function (DATA,
       FUN = function(x)
         t(x[[1]])
     )
-  
+
   Results_short <-
     as.data.frame(do.call(rbind, Results_short), stringsAsFactors = FALSE)
   Results_short[, 1:4] <-
     apply(Results_short[, 1:4], MARGIN = 2, as.numeric)
-  
+
   if(write_results) {
     if(!is.null(file_name)) {
       NAME_FILE <- file_name
     }else{
       NAME_FILE <- "IUCN_results"
     }
-    
-    if(write_file_option=="csv") 
+
+    if(write_file_option=="csv")
       utils::write.csv(Results_short, paste(getwd(),"/", NAME_FILE, ".csv", sep=""))
-    
+
     if(write_file_option=="excel") {
       Results_short <- data.frame(taxa=rownames(Results_short), Results_short)
       writexl::write_xlsx(Results_short, path = paste(getwd(),"/", NAME_FILE, ".xlsx", sep=""))
     }
-    
+
   }
-  
+
   if(!export_shp) {
-    
+
     Results <- Results_short
-    
+
     if(length(list_data)>5) {
-      
+
       print("Number of species per category")
       print(table(Results[,"Category_CriteriaB"]))
       print("Ratio of species per category")
       print(round(table(Results[,"Category_CriteriaB"])/nrow(Results)*100,1))
-      
+
     }
   }
-  
+
   return(Results)
 }
